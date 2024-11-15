022e7218a14300867b3d175b11048e5406938776
pedr00057 <132163618+pedr00057@users.noreply.github.com>
2024-11-15 15:09:56 -0600

    Subir Archivos

---
+++ Serie.py
@@ -0,0 +1,11 @@
+def consecutivos(a,b):
+    lista = []
+    for i in range (a,b +1):
+        lista.append(i)
+    return lista
+
+def reversa(a,b):
+    lista = []
+    for i in range (b,a-1,-1):
+        lista.append(i)
+    return lista
